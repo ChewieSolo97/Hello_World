7ceb44382df4c8b767eded548247e8cce48e1450
ChewieSolo97 <Mjdonaghy8264@eagle.fgcu.edu>
2018-08-29 11:19:49 -0400

    main

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("123" + 45);
+        Double stuff = 1.0;
+        System.out.println(stuff);
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        int input = scan.nextInt();
+        System.out.println("Please enter a base");
+        int base = scan.nextInt();
+        System.out.println("Please enter a base to convert to");
+        int convertBase = scan.nextInt();
+
+    }
+}
